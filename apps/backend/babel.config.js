f1f4bfba14b64df4433fd000be3b4a91ce5ae089
Diogo <diogoangelim@gmail.com>
2025-12-27 10:02:09 -0300

    chore(backend): update Babel config to support decorators and class properties

--- apps/backend/babel.config.js
+++ apps/backend/babel.config.js
@@ -6,6 +6,5 @@ module.exports = {
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    'istanbul',
   ],
 };
