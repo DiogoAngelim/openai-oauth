36c14aedcaeed1d53767f0eb34554d87148e1d49
Diogo <diogoangelim@gmail.com>
2025-12-27 12:18:17 -0300

    chore: remove previously tracked unwanted files now in .gitignore

--- apps/frontend/.next/static/chunks/app/onboarding/page-22723e97db406d86.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[461,931],{5599:function(n,e,u){Promise.resolve().then(u.t.bind(u,3348,23))}},function(n){n.O(0,[348,938,59,744],function(){return n(n.s=5599)}),_N_E=n.O()}]);
