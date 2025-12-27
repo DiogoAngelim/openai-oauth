20ac4537904020ca9a8cd3e91b0bbb60362231d3
Diogo <diogoangelim@gmail.com>
2025-12-27 08:55:05 -0300

    Build frontend: run production build in apps/frontend

--- apps/frontend/.next/static/chunks/app/page-95d2ca1b61e234d1.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,461],{2314:function(n,e,u){Promise.resolve().then(u.t.bind(u,4606,23))}},function(n){n.O(0,[606,957,19,744],function(){return n(n.s=2314)}),_N_E=n.O()}]);
